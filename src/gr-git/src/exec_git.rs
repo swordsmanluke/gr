a6f0932aae55f935810423f1ba4fc09d21fc76f7
Lucas <lucas@perfectlunacy.com>
2024-09-05 16:39:45 -0700

    Add: Don't send empty args to git

--- src/gr-git/src/exec_git.rs
+++ src/gr-git/src/exec_git.rs
@@ -10,7 +10,7 @@ impl ExecGit {
     }
 
     pub fn status(&self) -> Result<()> {
-        self.git("status", Vec::new())?;
+        self.git("status", vec![])?;
         Ok(())
     }
 
@@ -27,10 +27,12 @@ impl ExecGit {
     // TODO: git mergetool?
 
     fn git(&self, command: &str, args: Vec<String>) -> Result<()> {
-        let err = Command::new("git")
-            .arg(command)
-            .args(&*args)
-            .exec();
+        let mut cmd = Command::new("git");
+        cmd.arg(command);
+        if !args.is_empty() {
+            cmd.args(&*args);
+        }
+        let err = cmd.exec();
 
         // .exec means we should never get here.
         // our executable should be replaced by git
